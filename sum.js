fb347c872e30a47287bf366919c88d938712392e
Lokesh Regalla <43390163+Lokeshregalla@users.noreply.github.com>
2020-11-08 16:51:41 +1100

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,4 +1,4 @@
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
-const sum = num1 + num2;
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+const functions = {
+  add: (num1, num2) => num1 + num2
+}
+module.exports = functions;
